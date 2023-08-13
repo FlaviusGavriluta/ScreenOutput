fec69d42db12e40ef803d7e4531e6501eab1f675
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-08-13 12:02:13 +0300

    print the solution

---
+++ src/main/java/org/example/Solution.java
@@ -0,0 +1,19 @@
+package org.example;
+
+/*
+I think being a programmer is cool
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        // System.out.println("I think being a programmer is cool");
+        System.out.print("I ");
+        System.out.print("think ");
+        System.out.print("being ");
+        System.out.print("a ");
+        System.out.print("programmer ");
+        System.out.print("is ");
+        System.out.print("cool");
+    }
+}
